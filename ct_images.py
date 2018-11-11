9f4b2abadfea74323c4b2503d1895583d7f98b84
Igor Zyktin <nicord@yandex.ru>
2018-11-11 23:24:20 +0300

    density list added

--- ct_images.py
+++ ct_images.py
@@ -1,61 +1,66 @@
-import os
+"""
+    Images processing module
+"""
 import ct_files
 from ct_palette import palette
+from ct_palette import density_list
 from PIL import Image, ImageDraw, ImageFont
-# Based on https://gist.github.com/BigglesZX/4016539
-# Based on https://gist.github.com/almost/d2832d0998ad9dfec2cacef934e7d247
 
 TILE_HEIGHT = 16
 TILE_WIDTH = 16
 FONT_SIZE = 20
 
 
-def generate_symbol(density, color):
-    image = Image.new(mode="RGBA", size=(TILE_WIDTH, TILE_HEIGHT), color=(0, 0, 0, 255))
+def calculate_new_size(size, file_dict):
+    """
+        Downscaling the file
+    """
+    scale = file_dict.get('scale', 6)
+    input_width, input_height = size
+    output_height = input_height // scale
+    output_width = input_width // scale
+    return output_width, output_height
+
+
+def make_image(source_image, file_dict):
+    """
+        Main computation cycle
+    """
+    width, height = calculate_new_size(source_image.size, file_dict)
+    source_image = source_image.resize((width, height), 0)
+    source_image = source_image.convert(mode='RGB')
+
+    output_height = height * TILE_HEIGHT
+    output_width = width * TILE_WIDTH
+    output_image = Image.new(mode='RGB', size=(output_width, output_height), color=(0, 0, 0))
+
     font = ImageFont.truetype('Anonymous.ttf', FONT_SIZE)
-    textdraw = ImageDraw.ImageDraw(image, "RGBA")
-    textdraw.text((2, 0), text=palette[density], font=font, fill=color)
-    return image
 
-
-def base_conversion(source, size):
-    width, height = size
-    source = source.resize((int(size[0]/5), int(size[1]/5)), 0)
-    width = int(size[0]/5)
-    height = int(size[1]/5)
-    image_data = source.getdata()
-    color_space = len(image_data[0])
-
-    black = (0, 0, 0, 255)
-    image = Image.new(mode='RGBA', size=(width * TILE_WIDTH, height * TILE_HEIGHT), color=black)
     position = 0
-    symbols = {}
 
     for y in range(height):
         for x in range(width):
-            rgb_color = image_data[position]
-            density = int(sum(rgb_color) / color_space / 255 * 100)
+            """ Creating new symbolic tile  """
+            rgb_color = source_image.getpixel((x, y))
+            density = density_list[sum(rgb_color)]
 
-            if density in symbols:
-                symbol = symbols[density]
-            else:
-                symbol = generate_symbol(density, rgb_color)
-                symbols[density] = symbol
+            text_tile = Image.new(mode="RGB", size=(TILE_WIDTH, TILE_HEIGHT), color=(0, 0, 0))
+            textdraw = ImageDraw.ImageDraw(text_tile, "RGB")
+            textdraw.text((2, 0), text=palette[density], font=font, fill=rgb_color)
 
-            image.paste(symbol, (x * TILE_WIDTH, y * TILE_HEIGHT))
+            output_image.paste(text_tile, (x * TILE_WIDTH, y * TILE_HEIGHT))
+
             position += 1
 
-    return image
+    return output_image
 
 
 def analyse_image(image):
     """
-    Pre-process pass over the image to determine the mode (full or additive).
-    Need to know the mode before processing all frames.
+        Pre-process pass over the image to determine the mode (full or additive).
+        Need to know the mode before processing all frames.
     """
     results = {'size': image.size, 'mode': 'full'}
-    print(image.n_frames)
-    print(dir(image))
     try:
         while True:
             if image.tile:
@@ -71,61 +76,56 @@ def analyse_image(image):
     return results
 
 
-def make_frames(filename):
+def make_frames(source_image, file_dict):
     """
-    Iterate the GIF, extracting each frame.
+        Extract frames from GIF
     """
-    image = Image.open(os.path.join(ct_files.INPUT_PATH, filename))
-    frames = [filename]
-    size = image.size
-    #mode = analyse_image(image)['mode']
-    gif_palette = image.getpalette()
-    last_frame = image.convert('RGBA')
+    mode = analyse_image(source_image)['mode']
+    gif_palette = source_image.getpalette()
+    last_frame = None
+    total_frames = source_image.n_frames
 
-    for x in range(image.n_frames + 1):
+    frames = []
+
+    for x in range(total_frames + 1):
+        print(f'\rFrame: {str(x)} of {str(source_image.n_frames)}', end='')
         try:
-            image.seek(x)
+            if not source_image.getpalette():
+                source_image.putpalette(gif_palette)
 
-            empty_frame = Image.new('RGBA', (int(size[0]/200), int(size[1]/200)))
-            smaller = image.resize((int(size[0]/200), int(size[1]/200)), 0)
-            empty_frame.paste(smaller, (0, 0), smaller.convert('RGBA'))
-            new_frame = base_conversion(empty_frame, empty_frame.size)
-            #new_frame.show()
+            source_image.seek(x)
+            empty_frame = Image.new('RGB', source_image.size)
+
+            if mode == 'partial' and last_frame:
+                empty_frame.paste(last_frame, (0, 0))
+
+            empty_frame.paste(source_image, (0, 0))
+            new_frame = make_image(source_image=empty_frame, file_dict=file_dict)
+            last_frame = new_frame
             frames.append(new_frame)
         except EOFError:
             break
-            pass
-    return frames
-
-    try:
-        while True:
-            if not image.getpalette():
-                image.putpalette(gif_palette)
-
-            empty_frame = Image.new('RGBA', image.size)
-            if mode == 'partial':
-                empty_frame.paste(last_frame)
-
-            empty_frame.paste(image, (0, 0), image.convert('RGBA'))
-            new_frame = base_conversion(empty_frame, empty_frame.size)
-            frames.append(new_frame)
-            last_frame = new_frame
-            image.seek(image.tell() + 1)
-
-    except EOFError:
-        pass
-
+    print()
     return frames
 
 
-def convert_image(filename):
-    new_image = Image.open(os.path.join(ct_files.INPUT_PATH, filename))
-    converted_image = base_conversion(source=new_image, size=new_image.size)
-    ct_files.save_image(filename, converted_image)
+def convert(file_dict):
+    """
+        Loading file for image
+    """
+    status = 0
 
+    with open(file_dict['path'], mode='rb') as file:
+        image = Image.open(file)
 
-def convert_gif(filename):
-    temp_files = make_frames(filename)
-    ct_files.save_gif(temp_files)
+        if file_dict.get('ext') == 'gif':
+            frames = make_frames(source_image=image, file_dict=file_dict)
+            status = ct_files.save_gif(file_dict, frames)
 
+        elif file_dict.get('ext') in ['bmp', 'jpg', 'png']:
+            converted_image = make_image(source_image=image, file_dict=file_dict)
+            status = ct_files.save_image(file_dict, converted_image)
+
+    ct_files.move_file(file_dict)
+    return status
 
