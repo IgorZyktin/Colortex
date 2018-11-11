20bf43cad0ecd6cace0ccc93c25630cc5d181ce4
Igor Zyktin <nicord@yandex.ru>
2018-11-11 23:24:55 +0300

    scales added

--- ct_files.py
+++ ct_files.py
@@ -2,76 +2,134 @@
     File processing module
 """
 import os
-from PIL import Image
+import re
 PATH = os.path.abspath(os.path.curdir)
 INPUT_PATH = os.path.join('input', '')
 OUTPUT_PATH = os.path.join('output', '')
+USED_PATH = os.path.join('used', '')
 
 
-def get_files():
-    """ Retirve local files"""
-    filenames = os.listdir(INPUT_PATH)
+def extract_scale(name):
+    """
+        Extract scale out of the filename
+        For example: "[10] some_file.png" --> 10
+        For example: "[5-8] some_file.png" --> 5, 6, 7, 8
+    """
+    pattern = r'(?<=\[).+?(?=\])'
+    data = re.search(pattern, name)
 
-    gifs = []
-    imgs = []
+    if data is not None:
+        input_number = data.group()
+
+        if '-' in input_number:
+            start = input_number.split('-')[0]
+            end = input_number.split('-')[-1]
+            if start.isdigit() and end.isdigit():
+                return list(range(int(start), int(end) + 1))
+
+        elif input_number.isdigit():
+            return [int(input_number)]
+
+    return [6]
+
+
+def get_filenames(filenames=None):
+    """
+        Search for local files
+    """
+
+    if not filenames:
+        filenames = os.listdir(INPUT_PATH)
+        filenames = map(str.lower, filenames)
+
+    verified_files = []
 
     for filename in filenames:
         full_path = os.path.join(INPUT_PATH, filename)
 
         if os.path.exists(full_path):
-            if full_path.lower().endswith('gif'):
-                gifs.append(filename)
-            else:
-                imgs.append(filename)
 
-    if imgs:
-        print(f'Images found: {len(imgs)}')
-        for i, image in enumerate(imgs, start=1):
-            num = str(i).rjust(len(str(len(imgs))))
-            print(f'{num}. {image}')
+            name = filename.split('.')[0]
+            ext = filename.split('.')[-1]
+
+            scales_list = extract_scale(name)
+
+            pos = name.find(']')
+            name = name[pos + 2:]
+
+            for subscale in scales_list:
+                verified_files.append({
+                    'scale': subscale,
+                    'path': full_path,
+                    'name': '[' + str(subscale).rjust(2, '0') + 'x] ' + name,
+                    'ext': ext,
+                    'filename': filename
+                })
+
+    if verified_files:
+        print(f'Images in queue: {len(verified_files)}')
+        for i, image in enumerate(verified_files, start=1):
+            num = str(i).rjust(len(str(len(verified_files))))
+            print(f'{num}. {image["name"]}.{image["ext"]}')
         print()
 
-    if gifs:
-        print(f'Gif images found: {len(gifs)}')
-        for i, gif in enumerate(gifs, start=1):
-            num = str(i).rjust(len(str(len(gifs))))
-            print(f'{num}. {gif}')
-        print()
-
-    return imgs, gifs
+    return verified_files
 
 
 def unique_name(old_name, ext):
-    """ Avoid overwriting """
+    """
+        Avoid overwriting
+    """
     var = 1
-    filepath = OUTPUT_PATH
+    path = OUTPUT_PATH
     new_name = old_name
-    if os.path.isfile(os.path.join(filepath, new_name + '.' + ext)):
-        while os.path.isfile(os.path.join(filepath, new_name + '.' + ext)):
+    if os.path.isfile(os.path.join(path, new_name + '.' + ext)):
+        while os.path.isfile(os.path.join(path, new_name + '.' + ext)):
             var += 1
             new_name = old_name + '_(' + str(var) + ')'
     return new_name + '.' + ext
 
 
-def save_image(filename, image):
+def move_file(file_dict):
+    """
+        Move file from 'input' to 'used'
+    """
+    return
+    source = os.path.join(PATH, INPUT_PATH, file_dict['filename'])
+    resulting_name = unique_name(file_dict['name'], file_dict['ext'])
+    destination = os.path.join(PATH, USED_PATH, resulting_name)
+
+    if not os.path.exists(os.path.join(PATH, USED_PATH)):
+        os.mkdir(os.path.join(PATH, USED_PATH))
+
+    # if os.path.exists(destination):
+    #     os.remove(destination)
+    os.rename(source, destination)
+
+
+def save_image(file_dict, image):
+    """
+        Saving png
+    """
     if not image:
         return 0
 
-    name = filename.split('.')[0]
-    ext = filename.split('.')[-1]
-    new_name = unique_name(name, ext)
-    image.save(os.path.join(OUTPUT_PATH, new_name), ext)
-    print(f'Saved: {new_name}')
+    new_name = unique_name(file_dict['name'], 'png')
+    image.save(os.path.join(OUTPUT_PATH, new_name))
+    print(f'File saved: {new_name}')
+    return 1
 
 
-def save_gif(frames):
+def save_gif(file_dict, frames):
+    """
+        Saving gif
+    """
     if not frames:
         return 0
-    print(frames)
-    frames[1].show()
-    old_name = frames[0].split('.')[0]
-    del frames[0]
-    new_name = unique_name(old_name, 'gif')
-    full_name = os.path.join(OUTPUT_PATH, new_name + '.gif')
+
+    new_name = unique_name(file_dict['name'], 'gif')
+    full_name = os.path.join(OUTPUT_PATH, new_name)
     frames[0].save(full_name, save_all=True, append_images=frames[1:], duration=100, loop=0)
-    print(f'Saved: {new_name}')
+    print(f'GIF image file saved: {new_name}')
+    return 1
+
